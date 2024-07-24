18c7b4be5122206859b929d27a89b2238fee4e1f
Safna-123 <ssafna327@gmail.com>
2024-07-24 17:47:46 +0530

    Photo-app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Index from './components/Index';
+import Navbar from './components/Navbar';
+import Search from './components/Search';
+import View from './components/View';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Index/>}/>
+      <Route path='/navbar' element={<Navbar/>}/>
+      <Route path='/search' element={<Search/>}/>
+      <Route path='/view' element={<View/>}/>
+
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
